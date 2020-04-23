f7638010f0c0ea1d06bb6f01569cc97e9d594a30
Mark F <the.mark.fullton@gmail.com>
2020-04-22 20:31:02 -0700

    Added 12-ish words to dictionary

---
+++ assets/JS/dictionary.js
@@ -0,0 +1,123 @@
+var fears = {
+    acaro: {
+        word: "acaro",
+        desc: "fear of itching",
+        error: 6,
+        pic: ,
+    },
+    achluo: {
+        word: "achluo",
+        desc: "fear of darkness",
+        error: 4,
+        pic: ,
+    },
+    acro: {
+        word: "acro",
+        desc: "fear of spiders",
+        error: 3,
+        pic: ,
+    },
+    aero: {
+        word: "aero",
+        desc: "fear of flying",
+        error: 2,
+        pic: ,
+    },
+    alektoro: {
+        word: "alektoro",
+        desc: "fear of chickens",
+        error: 4,
+        pic: ,
+    },
+    algo: {
+        word: "algo",
+        desc: "fear of pain",
+        error: 5,
+        pic: ,
+    },
+    arachno: {
+        word: "arachno",
+        desc: "fear of spiders",
+        error: 3,
+        pic: ,
+    },
+    arachno: {
+        word: "arachno",
+        desc: "fear of spiders",
+        error: 3,
+        pic: ,
+    },
+    astra: {
+        word: "astra",
+        desc: "fear of thunder/lightning",
+        error: 4,
+        pic: ,
+    },
+    auto: {
+        word: "auto",
+        desc: "fear of being alone",
+        error: 3,
+        pic: ,
+    },
+    claustro: {
+        word: "claustro",
+        desc: "fear of confined spaces",
+        error: 4,
+        pic: ,
+    },
+    cryo: {
+        word: "cryo",
+        desc: "fear of ice/cold",
+        error: 4,
+        pic: ,
+    },
+    cyno: {
+        word: "cyno",
+        desc: "fear of dogs",
+        error: 4,
+        pic: ,
+    },
+    hemo: {
+        word: "hemo",
+        desc: "fear of blood",
+        error: 2,
+        pic: ,
+    },
+    hydro: {
+        word: "hydro",
+        desc: "fear of water",
+        error: 2,
+        pic: ,
+    },
+    neo: {
+        word: "neo",
+        desc: "fear of anything new",
+        error: 3,
+        pic: ,
+    },
+    nepho: {
+        word: "nepho",
+        desc: "fear of clouds",
+        error: 4,
+        pic: ,
+    },
+    onomato: {
+        word: "onomato",
+        desc: "fear of names",
+        error: 5,
+        pic: ,
+    },
+    ophidio: {
+        word: "ophidio",
+        desc: "fear of snakes",
+        error: 4,
+        pic: ,
+    },
+    pogono: {
+        word: "pogono",
+        desc: "fear of beards",
+        error: 4,
+        pic: ,
+    },
+    
+}
