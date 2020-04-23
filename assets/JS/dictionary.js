b968844ba54d3be406e4b44cf902eb71ddd271d8
Mark F <the.mark.fullton@gmail.com>
2020-04-22 23:26:51 -0700

    Created random word selection function

--- assets/JS/dictionary.js
+++ assets/JS/dictionary.js
@@ -1,123 +1,102 @@
-var fears = {
-    acaro: {
+var fears = [
+    acaro = {
         word: "acaro",
         desc: "fear of itching",
         error: 6,
-        pic: ,
     },
-    achluo: {
+    achluo = {
         word: "achluo",
         desc: "fear of darkness",
         error: 4,
-        pic: ,
     },
-    acro: {
+    acro = {
         word: "acro",
         desc: "fear of spiders",
         error: 3,
-        pic: ,
     },
-    aero: {
+    aero = {
         word: "aero",
         desc: "fear of flying",
         error: 2,
-        pic: ,
     },
-    alektoro: {
+    alektoro = {
         word: "alektoro",
         desc: "fear of chickens",
         error: 4,
-        pic: ,
     },
-    algo: {
+    algo = {
         word: "algo",
         desc: "fear of pain",
         error: 5,
-        pic: ,
     },
-    arachno: {
+    arachno = {
         word: "arachno",
         desc: "fear of spiders",
         error: 3,
-        pic: ,
     },
-    arachno: {
+    arachno = {
         word: "arachno",
         desc: "fear of spiders",
         error: 3,
-        pic: ,
     },
-    astra: {
+    astra = {
         word: "astra",
         desc: "fear of thunder/lightning",
         error: 4,
-        pic: ,
     },
-    auto: {
+    auto = {
         word: "auto",
         desc: "fear of being alone",
         error: 3,
-        pic: ,
     },
-    claustro: {
+    claustro = {
         word: "claustro",
         desc: "fear of confined spaces",
         error: 4,
-        pic: ,
     },
-    cryo: {
+    cryo = {
         word: "cryo",
         desc: "fear of ice/cold",
         error: 4,
-        pic: ,
     },
-    cyno: {
+    cyno = {
         word: "cyno",
         desc: "fear of dogs",
         error: 4,
-        pic: ,
     },
-    hemo: {
+    hemo = {
         word: "hemo",
         desc: "fear of blood",
         error: 2,
-        pic: ,
     },
-    hydro: {
+    hydro = {
         word: "hydro",
         desc: "fear of water",
         error: 2,
-        pic: ,
     },
-    neo: {
+    neo = {
         word: "neo",
         desc: "fear of anything new",
         error: 3,
-        pic: ,
     },
-    nepho: {
+    nepho = {
         word: "nepho",
         desc: "fear of clouds",
         error: 4,
-        pic: ,
     },
-    onomato: {
+    onomato = {
         word: "onomato",
         desc: "fear of names",
         error: 5,
-        pic: ,
     },
-    ophidio: {
+    ophidio = {
         word: "ophidio",
         desc: "fear of snakes",
         error: 4,
-        pic: ,
     },
-    pogono: {
+    pogono = {
         word: "pogono",
         desc: "fear of beards",
         error: 4,
-        pic: ,
-    },
-    
-}
+    },   
+]
--- assets/JS/game.js
+++ assets/JS/game.js
@@ -1,5 +1,13 @@
+    let answer ='';
+    let maxWrong = 0;
+    let mistakes = 0;
+    let guessed = [];
+    let wordStatus = null;
+    let totalRight = 0;
+
     let game = {
-    
+        
+
         instructions: function(){
             let getExpositionText = document.querySelector("#expositionText");
             getExpositionText.innerHTML = '<h3>How to Play:</h3><p>Try to guess the name of the fear before it gets you.</p><p>A description of the fear can be found in the bottom left-hand column. In the bottom right, you can see the word that needs to be guessed.</p><p>Press any key to guess whether the word has that letter in it. If you guess wrong, the fear will begin to manifest. Different fears take differing number of turns to manifest. The number of guesses you have left will be displayed in the lower right.</p><p>Your goal is to get <span class="sName">Shiver</span> to the other side of the map.</p><h4>Good luck!</h4></p><br><button type="button" class="btn btn-success  btn-lg btn-block" onclick="game.hideInstr(); game.main()">Play Game</button>';
@@ -7,6 +15,8 @@
 
         setGameUp: function(){
             this.hideInstr();
+            this.selectSetting();
+            this.wordSetUp();
             this.main();
         },
 
@@ -14,6 +24,21 @@
             let getExposition = document.querySelector("#exposition");
             getExposition.style.display = 'none';
         },
+
+        selectSetting: function(){
+
+        }
+
+        wordSetUp: function(){
+            let getFearPic = document.querySelector("#fearPic");
+            let getfearBox = document.querySelector("#fearBox");
+            let getNameBox = document.querySelector("#Name Box");
+            let getExposition = document.querySelector("#remGuessBox");
+            
+            answer = dictionary[Math.floor(Math.random() * dictionary.length)];
+
+
+        }
 
         main: function() {
             document.onkeyup = function(event) {
