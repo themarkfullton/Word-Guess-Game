51de300614788de578de449083672a3220aab804
Mark F <the.mark.fullton@gmail.com>
2020-04-24 13:27:49 -0700

    Made background change with the active fear

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -28,7 +28,6 @@
             
     
             this.hideInstr();
-            this.selectSetting();
             this.writeScore();
             this.wordSetUp();
             getGuessBox.style.backgroundColor = "#5b8c85";
@@ -45,62 +44,36 @@
 
         },
 
-        selectSetting: function(){
-            let getDungeonBox = document.querySelector("#dungeonBox");
-            let getPlaceTitle = document.querySelector("#placeTitle");
-            let getDialogue = document.querySelector("#dialogue");
-
-            setting = Math.floor(Math.random() * 4);
-
-            getDialogue.style.backgroundColor = "black";
-            getDialogue.style.borderRadius = "0 0 25px 25px";
-
-            switch(setting) {
-                case 0:
-                    getDungeonBox.style.backgroundImage = 'url("assets/images/Settings/set-city.jpg")';
-                    getPlaceTitle.innerHTML = "<h3>Abandonned Street</h3>";
-                    getDialogue.innerHTML = '<p>On the way home, <span class="sName">Shiver</span> feels someone watching them...</p>';
-                    break;
-                case 1:
-                    getDungeonBox.style.backgroundImage = 'url("assets/images/Settings/set-forest.jpg")';
-                    getPlaceTitle.innerHTML = "<h3>Forgotten Forest</h3>";
-                    getDialogue.innerHTML = '<p><span class="sName">Shiver</span> awakes from their sleep only time find themselves in a forboding forest...</p>';
-                    break;
-                case 2:
-                    getDungeonBox.style.backgroundImage = 'url("assets/images/Settings/set-hospital.jpg")';
-                    getPlaceTitle.innerHTML = "<h3>Twisted Hospital</h3>";
-                    getDialogue.innerHTML = '<p>Although they intended to walk home, <span class="sName">Shiver\'s</span> feet took them to this abandonned hospital...</p>';
-                    break;
-                case 3:
-                    getDungeonBox.style.backgroundImage = 'url("assets/images/Settings/set-house.jpg")';
-                    getPlaceTitle.innerHTML = "<h3>Living Mansion</h3>";
-                    getDialogue.innerHTML = '<p>Who lives here? <span class="sName">Shiver</span> certainly doesn\'t... Though they woke up here all the same...</p>';
-                    break;
-                case 4:
-                    getDungeonBox.style.backgroundImage = 'url("assets/images/Settings/set-village.jpg")';
-                    getPlaceTitle.innerHTML = "<h3>Fishing Village</h3>";
-                    getDialogue.innerHTML = '<p>While on a cross-country trip, <span class="sName">Shiver</span> found themselves checking out this old, New England town...</p>';
-                    break;
-            }
-        },
-
         writeScore: function(){
             let getScoreBox = document.querySelector("#scoreBox");
             getScoreBox.innerHTML = "<h4>Defeated " + numDefeat + " of 10</h4>";
         },
 
         wordSetUp: function(){
-            let getFearPic = document.querySelector("#fearPic");
             let getFearBox = document.querySelector("#fearBox");
             var indexNum = Math.floor(Math.random() * dictionary.length);
 
             answer = dictionary[indexNum];
             lives = dictionary[indexNum].error;
 
-            getFearPic.innerHTML = '<img alt="fear" src="assets/Fears/'+ indexNum +'0>';
             getFearBox.innerHTML = '<h5>' + answer.desc + '</h5>'
+            this.selectSetting(answer.badge, answer.dia);
             this.printHangLines();
             this.printRemGuess(lives);
+        },
+
+        selectSetting: function(badge, dia){
+            let getDungeonBox = document.querySelector("#dungeonBox");
+            let getDialogue = document.querySelector("#dialogue");
+
+            getDialogue.style.backgroundColor = "black";
+            getDialogue.style.borderRadius = "0 0 25px 25px";
+
+            getDungeonBox.style.backgroundImage = "url('assets/Images/Backgrounds/" + badge + ".png')";
+            getDungeonBox.style.backgroundSize = "cover";
+
+            getDialogue.innerHTML = '<p>' + dia + '</p>';
+            
         },
 
         printHangLines: function(){
